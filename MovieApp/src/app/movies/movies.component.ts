e9f27a1a3207922ee6d37a6b57a9058f49844a34
Emre Tetik <emre.tetik@elekse.com>
2023-04-13 04:56:48 +0300

    add movie service

--- MovieApp/src/app/movies/movies.component.ts
+++ MovieApp/src/app/movies/movies.component.ts
@@ -1,21 +1,29 @@
-import { Component } from "@angular/core";
-import { Movie } from "../movie";
-import { Movies } from "../movie.datasource";
+import { Component } from '@angular/core';
+import { Movie } from '../movie';
+import { MovieService } from '../movie.service';
 
 @Component({
-    selector:'movies',
-    templateUrl:'movies.component.html'
+  selector: 'movies',
+  templateUrl: 'movies.component.html',
 })
-export class MoviesComponent{
-    title = 'Movie List';
-    movies = Movies;
-    selectedMovie : Movie;
-    
-    constructor() {
-        this.selectedMovie = new Movie();
-    }
+export class MoviesComponent {
+  title = 'Movie List';
+  movies: Movie[];
+  selectedMovie: Movie;
 
-    onSelect(movie:Movie) :void {
-        this.selectedMovie = movie;
-    }
+  constructor(private movieService:MovieService) {}
+
+  ngOnInit(): void {
+    this.getMovies();
+  }
+
+  onSelect(movie: Movie): void {
+    this.selectedMovie = movie;
+  }
+
+  getMovies(): void {
+    this.movieService.getMovies().subscribe((movies) => {
+      this.movies = movies;
+    });
+  }
 }
